705e5dd2c9e23ba81cecc8faf8a34f5916dc1bde
Martin Coimbra <martin.coimbra.2011@hotmail.com>
2020-05-01 16:33:16 -0300

    BaseDeDatos

---
+++ tareaBaseDeDatos/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.cbserie = New System.Windows.Forms.ComboBox()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.grdTabla = New System.Windows.Forms.DataGridView()
+        CType(Me.grdTabla, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Apellido:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 154)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Serie"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(113, 42)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(121, 20)
+        Me.txtNombre.TabIndex = 3
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(113, 90)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(121, 20)
+        Me.txtApellido.TabIndex = 4
+        '
+        'cbserie
+        '
+        Me.cbserie.FormattingEnabled = True
+        Me.cbserie.Items.AddRange(New Object() {"La Casa de Papel", "Poco Ortodoxa", "GOT", "Stranger Things", "Ninguna de las anterioes"})
+        Me.cbserie.Location = New System.Drawing.Point(113, 146)
+        Me.cbserie.Name = "cbserie"
+        Me.cbserie.Size = New System.Drawing.Size(121, 21)
+        Me.cbserie.TabIndex = 5
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(40, 208)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 29)
+        Me.btnAgregar.TabIndex = 6
+        Me.btnAgregar.Text = "Agregar datos"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(159, 208)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 29)
+        Me.btnSalir.TabIndex = 7
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'grdTabla
+        '
+        Me.grdTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdTabla.Location = New System.Drawing.Point(260, 24)
+        Me.grdTabla.Name = "grdTabla"
+        Me.grdTabla.Size = New System.Drawing.Size(443, 213)
+        Me.grdTabla.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 308)
+        Me.Controls.Add(Me.grdTabla)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.cbserie)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Ingreso de datos"
+        CType(Me.grdTabla, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents cbserie As ComboBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents grdTabla As DataGridView
+End Class
---
+++ tareaBaseDeDatos/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Dim conexion As MySqlConnection
+        conexion = New MySqlConnection
+        Dim cmd As New MySqlCommand
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "server=localhost; database=encuesta;Uid=root;Pwd=;"
+
+
+
+        If txtNombre.Text = "" Or txtApellido.Text = "" Or cbserie.Text = "" Then
+
+            MessageBox.Show("Error, ingrese los datos correspondientes", "Datos ERRONEOS", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        Else
+            Try
+
+                ' MsgBox("Conectado con la base de datos.")
+
+                conexion.Open()
+
+                cmd.Connection = conexion
+                cmd.CommandText = "INSERT INTO encuesta_series(nombre, apellido, serie_favorita) VALUES (@nombre, @apellido, @serie_favorita)"
+                cmd.Prepare()
+
+                cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+                cmd.Parameters.AddWithValue("@apellido", txtApellido.Text)
+                cmd.Parameters.AddWithValue("@serie_favorita", cbserie.Text)
+                cmd.ExecuteNonQuery()
+
+                cmd.CommandText = "SELECT * FROM encuesta_series ORDER BY id ASC"
+                adaptador.SelectCommand = cmd
+                adaptador.Fill(ds, "Tabla")
+                grdTabla.DataSource = ds
+                grdTabla.DataMember = "Tabla"
+
+                conexion.Close()
+                txtNombre.Clear()
+                txtApellido.Clear()
+                cbserie.Text = ""
+                MsgBox("Datos igresados.")
+
+            Catch ex As Exception
+                MsgBox("No ha conectado con la base de datos.")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
